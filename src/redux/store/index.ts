cf3897c4d968cc8ac691d98ee78baaceefd9f790
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-09 17:15:59 +0200

    Add sagas to store config

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -4,13 +4,13 @@ import rootReducer from '../reducers/rootReducer';
 import { StoreState } from './types';
 
 import createSagaMiddleware from 'redux-saga';
-// import albunsSaga from './sagas/sagas';
+import albunsSaga from '../sagas/postsSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore() {
-	const store = createStore<StoreState, LoadingType, any, any>(rootReducer /* , applyMiddleware(sagaMiddleware) */);
-	/* sagaMiddleware.run(albunsSaga); */
+	const store = createStore<StoreState, LoadingType, any, any>(rootReducer, applyMiddleware(sagaMiddleware));
+	sagaMiddleware.run(albunsSaga);
 
 	return store;
 }
