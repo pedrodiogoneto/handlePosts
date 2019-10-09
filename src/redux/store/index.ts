a9b43fae31c4eb342c7cafc4d96e03916bbccb0e
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-09 12:27:44 +0200

    Add store and types

---
+++ src/redux/store/index.ts
@@ -0,0 +1,16 @@
+import { applyMiddleware, createStore } from 'redux';
+import { LoadingType } from '../actions/types';
+import rootReducer from '../reducers/rootReducer';
+import { StoreState } from './types';
+
+import createSagaMiddleware from 'redux-saga';
+// import albunsSaga from './sagas/sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export default function configureStore() {
+	const store = createStore<StoreState, LoadingType, any, any>(rootReducer /* , applyMiddleware(sagaMiddleware) */);
+	/* sagaMiddleware.run(albunsSaga); */
+
+	return store;
+}
