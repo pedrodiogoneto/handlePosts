f22039f7f688b96e0f6117a3cd86e36b92a363b7
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-09 23:15:55 +0200

    Add button to listCard

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -4,15 +4,17 @@ import { Post } from '../constants/types';
 interface Props {
 	post: Post;
 	key: number;
+	handleOnDelete: (id: number) => void;
 }
 
 const ListCard: FunctionComponent<any> = (props: Props) => {
-	const { post } = props;
+	const { post, handleOnDelete } = props;
 	return (
 		<div style={styles.wrapper}>
 			<p style={styles.text}>{post.title}</p>
 			<p style={styles.text}>{post.content}</p>
 			<img style={styles.image} src={post.image_url}/>
+			<button onClick={() => handleOnDelete(post.id)}>delete</button>
 		</div>
 	);
 };
