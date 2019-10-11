4f9281fdd3382bb72d4d0e937bf23a6f2eabe76a
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-11 14:47:37 +0200

    Externalize navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,24 +6,11 @@ interface NavbarProps {
 
 const Navbar: FunctionComponent<any> = (props: NavbarProps) => {
 	return (
-		<div style={styles.wrapper}>
+		<div className="navbar__wrapper">
 			<h3>POSTS</h3>
-			<button style={styles.button} onClick={() => props.handleOnClickAdd(true)}>Add</button>
+			<button className="navbar__button" onClick={() => props.handleOnClickAdd(true)}>Add</button>
 		</div>
 	);
 };
 
 export default Navbar;
-
-const styles = {
-	wrapper: {
-		borderBottom: '1px solid black',
-		display: 'flex',
-		justifyContent: 'space-between',
-		padding: '0 2%',
-	},
-	button: {
-		border: '0px',
-		backgroundColor: 'white',
-	}
-};
